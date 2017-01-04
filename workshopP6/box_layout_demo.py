a005f66791aa15571128ec9878e3fc38efe3c614
jc345932 <junnao.luo@my.jcu.edu.au>
2017-01-04 14:53:50 +0800

    correct one box_layout

--- workshopP6/box_layout_demo.py
+++ workshopP6/box_layout_demo.py
@@ -11,7 +11,8 @@ class BoxLayoutDemo(App):
         print("test")
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
     def handle_clear(self):
-        self.root.ids.output_label.text ="Enter your name"
+        self.root.ids.input_name.text =" "
+        self.root.ids.output_label.text=" "
 
 
 
